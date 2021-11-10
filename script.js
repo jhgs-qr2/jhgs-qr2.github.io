5a02255cc75e0aa5f8cde154f54bcbb87f493552
Will-Harmer <44799104+Will-Harmer@users.noreply.github.com>
2021-11-10 22:04:59 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,172 +1,184 @@
 var namearray = "";
 var foundname = "";
-function getCookie(cname) {
-  let name = cname + "=";
-  let decodedCookie = decodeURIComponent(document.cookie);
-  let ca = decodedCookie.split(';');
-  for(let i = 0; i <ca.length; i++) {
-    let c = ca[i];
-    while (c.charAt(0) == ' ') {
-      c = c.substring(1);
-    }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
-  }
-  return "";
-}
-function loadarray() {
-if (getCookie("names") == ""){
-     namearray = [["Name","Time"]];
-
-} else {
-    namearray = getCookie("names");
-}
-}
-window.onload = loadarray;
 let signedin = true;
 
-    function makeApiCall(qr, club) {
-        var params = {
-            // The ID of the spreadsheet to update.
-            spreadsheetId: '1SK2oTjri2krTIZ6XglLbBrv4PqZM-OiGf3f_WovTEZ4',
-
-            // The A1 notation of a range to search for a logical table of data.
-            // Values will be appended after the last row of the table.
-            range: club.concat('!A1:B1'),
-
-            // How the input data should be interpreted.
-            valueInputOption: 'USER_ENTERED',
-
-            // How the input data should be inserted.
-            insertDataOption: 'INSERT_ROWS',
-        };
-        var d = new Date();
-        var n = d.toLocaleString();
-        var signedin = false;
-        var valueRangeBody = {
-
-            "range": club + "!A1:B1",
-            "majorDimension": "ROWS",
-            "values": [
-                [qr, n]
-            ]
-        };
-
-        var request = gapi.client.sheets.spreadsheets.values.append(params, valueRangeBody);
-        request.then(function (response) {
-        }, function (reason) {
-            alert('error: ' + reason.result.error.message);
-        });
+function clubspickermaker(data) {
+    var sel = document.getElementById("clubPicked");
+    for (var i = 0; i < clubs.length; i++) {
+        var opt = document.createElement('option');
+        opt.innerHTML = clubs[i];
+        opt.value = clubs[i];
+        sel.appendChild(opt);
     }
+};
 
-    function initClient() {
-        var API_KEY = 'AIzaSyAm02zc16ma4fmsHi-a34Kcze0C9rc19wk';
-
-        var CLIENT_ID = '36530272431-up5uj8q5psvs60fafqaah8cbhdoj99dm.apps.googleusercontent.com';
-
-        var SCOPE = 'https://www.googleapis.com/auth/spreadsheets';
-
-        gapi.client.init({
-            'apiKey': API_KEY,
-            'clientId': CLIENT_ID,
-            'scope': SCOPE,
-            'discoveryDocs': ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
-        }).then(function () {
-            gapi.auth2.getAuthInstance().isSignedIn.listen(updateSignInStatus);
-            updateSignInStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-        });
-    }
-
-    function handleClientLoad() {
-        gapi.load('client:auth2', initClient);
-    }
-
-    function updateSignInStatus(isSignedIn) {
-        if (isSignedIn) {
-            document.getElementById("signin-button").style.display = "none";
-            document.getElementById("clubLabel").style.display = "initial";
-            document.getElementById("clubPicked").style.display = "initial";
-            document.getElementById("cookiesconsent").style.display = "none";
-            signedin = true;
-        } else {
-            document.getElementById("signedIn").innerHTML = "Signed out";
-            document.getElementById("clubLabel").style.display = "none";
-            document.getElementById("clubPicked").style.display = "none";
-            document.getElementById("signout-button").style.display = "none";
-            signedin = false;
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
         }
     }
+    return "";
+}
 
-    function handleSignInClick(event) {
-        gapi.auth2.getAuthInstance().signIn();
-                            document.getElementById("signout-button").style.display = "initial";
+function loadarray() {
+    if (getCookie("names") == "") {
+        namearray = [
+            ["Name", "Time"]
+        ];
 
+    } else {
+        namearray = getCookie("names");
     }
+}
+
+
+function makeApiCall(qr, club) {
+    var params = {
+        // The ID of the spreadsheet to update.
+        spreadsheetId: '1SK2oTjri2krTIZ6XglLbBrv4PqZM-OiGf3f_WovTEZ4',
+
+        // The A1 notation of a range to search for a logical table of data.
+        // Values will be appended after the last row of the table.
+        range: club.concat('!A1:B1'),
+
+        // How the input data should be interpreted.
+        valueInputOption: 'USER_ENTERED',
+
+        // How the input data should be inserted.
+        insertDataOption: 'INSERT_ROWS',
+    };
+    var d = new Date();
+    var n = d.toLocaleString();
+    var signedin = false;
+    var valueRangeBody = {
+
+        "range": club + "!A1:B1",
+        "majorDimension": "ROWS",
+        "values": [
+            [qr, n]
+        ]
+    };
+
+    var request = gapi.client.sheets.spreadsheets.values.append(params, valueRangeBody);
+    request.then(function(response) {}, function(reason) {
+        alert('error: ' + reason.result.error.message);
+    });
+}
+
+function initClient() {
+    var API_KEY = 'AIzaSyAm02zc16ma4fmsHi-a34Kcze0C9rc19wk';
+
+    var CLIENT_ID = '36530272431-up5uj8q5psvs60fafqaah8cbhdoj99dm.apps.googleusercontent.com';
+
+    var SCOPE = 'https://www.googleapis.com/auth/spreadsheets';
+
+    gapi.client.init({
+        'apiKey': API_KEY,
+        'clientId': CLIENT_ID,
+        'scope': SCOPE,
+        'discoveryDocs': ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
+    }).then(function() {
+        gapi.auth2.getAuthInstance().isSignedIn.listen(updateSignInStatus);
+        updateSignInStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+    });
+}
+
+function handleClientLoad() {
+    gapi.load('client:auth2', initClient);
+}
+
+function updateSignInStatus(isSignedIn) {
+    if (isSignedIn) {
+        document.getElementById("signin-button").style.display = "none";
+        document.getElementById("clubLabel").style.display = "initial";
+        document.getElementById("clubPicked").style.display = "initial";
+        document.getElementById("cookiesconsent").style.display = "none";
+        signedin = true;
+    } else {
+        document.getElementById("signedIn").innerHTML = "Signed out";
+        document.getElementById("clubLabel").style.display = "none";
+        document.getElementById("clubPicked").style.display = "none";
+        document.getElementById("signout-button").style.display = "none";
+        signedin = false;
+    }
+}
+
+function handleSignInClick(event) {
+    gapi.auth2.getAuthInstance().signIn();
+    document.getElementById("signout-button").style.display = "initial";
+
+}
+
 function setCookie(cname, cvalue, exdays) {
-  const d = new Date();
-  d.setTime(d.getTime() + (exdays*24*60*60*1000));
-  let expires = "expires="+ d.toUTCString();
-  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
-    function handleSignOutClick(event) {
-        gapi.auth2.getAuthInstance().signOut();
-                    document.getElementById("signin-button").style.display = "initial";
-    }
 
-    function addData(event) {
-      var duplicate = false;
-                             var qr = foundname;
+function handleSignOutClick(event) {
+    gapi.auth2.getAuthInstance().signOut();
+    document.getElementById("signin-button").style.display = "initial";
+}
 
-namearray.forEach(function (namedate, index) {
-if (namedate.indexOf(qr) > -1){
-duplicate = true;
-}
-}
-);
-        
-      
-      
-                             var qr = foundname;
-        if (signedin == false) {
-            alert("Not signed in")
-        } else if (qr == "") {
-            alert("No name found")
-        } else if (document.getElementById("clubPicked").value == "none") {
-            alert("No club selected")
-        } else if (duplicate) {
-            alert("QR already scanned today")
+function addData(event) {
+    var duplicate = false;
+    var qr = foundname;
+
+    namearray.forEach(function(namedate, index) {
+        if (namedate.indexOf(qr) > -1) {
+            duplicate = true;
         }
-        else{
+    });
 
-            var clubHTML = document.getElementById("clubPicked");
-            var club = clubHTML.value;
-          makeApiCall(qr, club);
-            var sound = new Howl({
-                src: ["correct.wav"]
-            }).play();
-namearray.push([qr,new Date().toLocaleTimeString()])
-            updateTable(namearray)
-            setCookie("names", namearray, 0.5)
-        }
- 
-    function sleep(milliseconds) {
-        const date = Date.now();
-        let currentDate = null;
-        do {
-            currentDate = Date.now();
-        } while (currentDate - date < milliseconds);
+
+
+    var qr = foundname;
+    if (signedin == false) {
+        alert("Not signed in")
+    } else if (qr == "") {
+        alert("No name found")
+    } else if (document.getElementById("clubPicked").value == "none") {
+        alert("No club selected")
+    } else if (duplicate) {
+        alert("QR already scanned today")
+    } else {
+
+        var clubHTML = document.getElementById("clubPicked");
+        var club = clubHTML.value;
+        makeApiCall(qr, club);
+        var sound = new Howl({
+            src: ["correct.wav"]
+        }).play();
+        namearray.push([qr, new Date().toLocaleTimeString()])
+        updateTable(namearray)
+        setCookie("names", namearray, 0.5)
     }
-    }
+
+}
+
+function qrscanner() {
     var out = "";
-    var scanner = new Instascan.Scanner({video: document.getElementById('preview'), scanPeriod: 5, mirror: false});
-    scanner.addListener('scan', function (content) {
+    var scanner = new Instascan.Scanner({
+        video: document.getElementById('preview'),
+        scanPeriod: 5,
+        mirror: false
+    });
+    scanner.addListener('scan', function(content) {
         // content = the found qr code
         out = content;
-foundname = out;
-      addData();
+        foundname = out;
+        addData();
     });
-    Instascan.Camera.getCameras().then(function (cameras) {
+    Instascan.Camera.getCameras().then(function(cameras) {
         if (cameras.length > 0) {
             scanner.start(cameras[0]);
             if (cameras[0] != "") {
@@ -180,46 +192,52 @@ foundname = out;
             alert('No cameras found.');
         }
     });
-   function updateTable(namearray) {
-        document.getElementById("table").innerHTML = "";
-        function makeTableHTML(myArray) {
-            var result = "<table>";
-            for (var i = 0; i < myArray.length; i++) {
-                result += "<tr>";
-                for (var j = 0; j < myArray[i].length; j++) {
-                    result += "<td>" + myArray[i][j] + "</td>";
-                }
-                result += "</tr>";
+}
+
+function updateTable(namearray) {
+    document.getElementById("table").innerHTML = "";
+
+    function makeTableHTML(myArray) {
+        var result = "<table>";
+        for (var i = 0; i < myArray.length; i++) {
+            result += "<tr>";
+            for (var j = 0; j < myArray[i].length; j++) {
+                result += "<td>" + myArray[i][j] + "</td>";
             }
-            result += "</table>";
-
-            return result;
+            result += "</tr>";
         }
+        result += "</table>";
 
-        tablediv = document.getElementById("table")
-        var tablecontents = makeTableHTML(namearray);
-        tablediv.insertAdjacentHTML('afterbegin', tablecontents);
+        return result;
     }
-    updateTable(namearray);
 
-    function clubspickermaker(data) {
-        var sel = document.getElementById("clubPicked");
-        for(var i = 0; i < clubs.length; i++) {
-            var opt = document.createElement('option');
-            opt.innerHTML = clubs[i];
-            opt.value = clubs[i];
-            sel.appendChild(opt);
-        }
-    };
+    tablediv = document.getElementById("table")
+    var tablecontents = makeTableHTML(namearray);
+    tablediv.insertAdjacentHTML('afterbegin', tablecontents);
+}
 
-    var clubs = "";
-    fetch('https://will-harmer.github.io/clubs.txt')
-        .then(response => response.text())
-        .then((data) => {
-            clubs = data;
-            clubs = data.split("\n");
-        })
-        .then((data) => {
-            clubspickermaker(clubs)
-        })
+function clubspickermaker(data) {
+    var sel = document.getElementById("clubPicked");
+    for (var i = 0; i < clubs.length; i++) {
+        var opt = document.createElement('option');
+        opt.innerHTML = clubs[i];
+        opt.value = clubs[i];
+        sel.appendChild(opt);
+    }
+};
+
+
+updateTable(namearray);
+qrscanner();
+loadarray()
+var clubs = "";
+fetch('https://will-harmer.github.io/clubs.txt')
+    .then(response => response.text())
+    .then((data) => {
+        clubs = data;
+        clubs = data.split("\n");
+    })
+    .then((data) => {
+        clubspickermaker(clubs)
+    })
 // bottom text
